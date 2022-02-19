920d05439e5f7ac1872c701f38345f3dd779dd47
Ethan Brooks <ethanabrooks@gmail.com>
2022-02-19 09:27:24 -0500

    use jupytext

--- readme.py
+++ readme.py
@@ -1,73 +1,72 @@
----
-jupyter:
-  jupytext:
-    formats: ipynb,py:percent,md
-    text_representation:
-      extension: .md
-      format_name: markdown
-      format_version: '1.3'
-      jupytext_version: 1.13.7
-  kernelspec:
-    display_name: Python 3 (ipykernel)
-    language: python
-    name: python3
----
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent,md
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.7
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
 
-# An alternative to `argparse` based on [Functional Pearls: Monadic Parsing in Haskell](https://www.cmi.ac.in/~spsuresh/teaching/prgh15/papers/monadic-parsing.pdf).
+# %% [markdown]
+# # An alternative to `argparse` based on [Functional Pearls: Monadic Parsing in Haskell](https://www.cmi.ac.in/~spsuresh/teaching/prgh15/papers/monadic-parsing.pdf).
 
-<!-- #region pycharm={"name": "#%% md\n"} -->
-Arguments
-<!-- #endregion -->
+# %% [markdown] pycharm={"name": "#%% md\n"}
+# Arguments
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 from monad_argparse import Argument
 
 Argument("name").parse_args("Ethan")
-```
 
-Flags
+# %% [markdown]
+# Flags
 
-```python
+# %%
 from monad_argparse import Flag
 
 Flag("verbose").parse_args("--verbose")
-```
 
-Options
+# %% [markdown]
+# Options
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 from monad_argparse import Option
 
 Option("value").parse_args("--value", "x")
-```
 
-Failure
+# %% [markdown]
+# Failure
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 Option("value").parse_args("--value")
-```
 
-Alternatives (or "Sums")
+# %% [markdown]
+# Alternatives (or "Sums")
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p = Flag("verbose") | Option("value")
 p.parse_args("--verbose")
-```
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p.parse_args("--value", "x")
-```
 
-Sequencing
+# %% [markdown]
+# Sequencing
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p = Argument("first") >> Argument("second")
 p.parse_args("a", "b")
-```
 
-This is shorthand for the following:
+# %% [markdown]
+# This is shorthand for the following:
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 from monad_argparse import Parser
 
 
@@ -78,51 +77,48 @@ def g():
 
 
 Parser.do(g).parse_args("a", "b")
-```
 
-Variable arguments
+# %% [markdown]
+# Variable arguments
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p = Argument("many").many()
 p.parse_args("a", "b")
-```
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p = (Flag("verbose") | Flag("quiet")).many()
 p.parse_args("--verbose", "--quiet")
-```
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p.parse_args("--quiet", "--verbose")
-```
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p.parse_args("--quiet")
-```
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p.parse_args("--quiet", "--quiet", "--quiet")
-```
 
-Combine sequences and sums
+# %% [markdown]
+# Combine sequences and sums
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p1 = Flag("verbose") | Flag("quiet") | Flag("yes")
 p = p1 >> Argument("a")
 p.parse_args("--verbose", "value")
-```
 
-What about doing this many times?
+# %% [markdown]
+# What about doing this many times?
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 p2 = p1.many()
 p = p2 >> Argument("a")
 p.parse_args("--verbose", "value")
-```
 
-The result is awkwardly nested. To deal with this, we use `Parser.do`:
 
-```python pycharm={"name": "#%%\n"}
+# %% [markdown]
+# The result is awkwardly nested. To deal with this, we use `Parser.do`:
+
+# %% pycharm={"name": "#%%\n"}
 def g():  # type: ignore[no-redef]
     xs = yield p2
     x = yield Argument("a")
@@ -130,11 +126,12 @@ def g():  # type: ignore[no-redef]
 
 
 Parser.do(g).parse_args("--verbose", "--quiet", "value")
-```
 
-A common pattern is to alternate checking for positional arguments with checking for non-positional arguments:
 
-```python pycharm={"name": "#%%\n"}
+# %% [markdown]
+# A common pattern is to alternate checking for positional arguments with checking for non-positional arguments:
+
+# %% pycharm={"name": "#%%\n"}
 def g():  # type: ignore[no-redef]
     xs1 = yield p2
     x1 = yield Argument("first")
@@ -145,11 +142,12 @@ def g():  # type: ignore[no-redef]
 
 
 Parser.do(g).parse_args("a", "--verbose", "b", "--quiet")
-```
 
-A simpler way to do this is with the `interleave` method:
 
-```python pycharm={"name": "#%%\n"}
+# %% [markdown]
+# A simpler way to do this is with the `interleave` method:
+
+# %% pycharm={"name": "#%%\n"}
 def g():  # type: ignore[no-redef]
     return (Flag("verbose") | Flag("quiet") | Flag("yes")).interleave(
         Argument("first"), Argument("second")
@@ -157,12 +155,11 @@ def g():  # type: ignore[no-redef]
 
 
 Parser.do(g).parse_args("a", "--verbose", "b", "--quiet")
-```
 
-or `build`:
+# %% [markdown]
+# or `build`:
 
-```python pycharm={"name": "#%%\n"}
+# %% pycharm={"name": "#%%\n"}
 Parser.build(
     Flag("verbose") | Flag("quiet") | Flag("yes"), Argument("first"), Argument("second")
 ).parse_args("a", "--verbose", "b", "--quiet")
-```
