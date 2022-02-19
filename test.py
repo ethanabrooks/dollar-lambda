5675a5836944c71cae2bb82eb78fb9b7251ed742
Ethan Brooks <ethanabrooks@gmail.com>
2022-02-19 07:41:50 -0500

    run test.py as part of pre-commit

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+#! /usr/bin/env python
 from monad_argparse import monad, parser
 
 if __name__ == "__main__":
     import doctest
 
-    doctest.testmod(monad)
-    doctest.testmod(parser)
+    fail_count, _ = doctest.testmod(monad)
+    assert fail_count == 0
+    fail_count, _ = doctest.testmod(parser)
+    assert fail_count == 0
