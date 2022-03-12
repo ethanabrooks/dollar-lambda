aca7fba5c9a945ae60abe038b35110888753fb70
Alec Newman <alec@rovaughn.com>
2022-03-12 18:53:45 -0500

    temp

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "TODO";
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+    pytypeclass.url = "path:/Users/alec/pytypeclass";
+  };
+  outputs = { self, nixpkgs, pytypeclass }:
+    (let systems = [ "x86_64-darwin" ];
+    in {
+      packages = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = (nixpkgs.lib.genAttrs systems
+            (system: import nixpkgs { inherit system; })).${system};
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = [ pkgs.python310 ];
+            shellHook = ''
+              [ -d .venv ] || python -m venv .venv
+              .venv/bin/pip install --upgrade pip
+              .venv/bin/pip install poetry
+              .venv/bin/poetry install
+            '';
+          };
+          dollar-lambda = (pkgs.python310Packages.buildPythonPackage {
+            name = "dollar-lambda";
+            src = ./.;
+            propagatedBuildInputs = [ pytypeclass.defaultPackage.${system} ];
+          });
+        });
+      defaultPackage = nixpkgs.lib.genAttrs systems
+        (system: self.packages.${system}.dollar-lambda);
+    });
+}
