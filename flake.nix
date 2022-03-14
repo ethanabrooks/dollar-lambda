b7e5f7607ca1d14b14200b35d164d5034d7fe303
Alec Newman <alec@rovaughn.com>
2022-03-14 11:17:00 -0400

    temp

--- flake.nix
+++ flake.nix
@@ -7,27 +7,30 @@
       in {
         devShell = pkgs.mkShell {
           buildInputs = [
-            pkgs.ninja
-            pkgs.git
-            pkgs.nixfmt
-            pkgs.shellcheck
-            pkgs.shfmt
             pkgs.fd
-            pkgs.rsync
+            pkgs.git
+            pkgs.ninja
+            pkgs.nixfmt
+            pkgs.just
             pkgs.nodePackages.prettier
             pkgs.python310
+            pkgs.rsync
+            pkgs.shellcheck
+            pkgs.shfmt
           ];
-          # There were some issues with automatically installing Poetry and
-          # mypy on macOS with Python 3.10 using pkgs.python310Packages, so in
-          # the shell hook we manually create a virtualenv, upgrade Pip,
-          # install Poetry, then use Poetry to install dependencies. This also
-          # allows someone to use Poetry to install this Python environment
-          # without using Nix.
+          # There were some issues with automatically installing Poetry and mypy
+          # on macOS with Python 3.10 using pkgs.python310Packages, so in the
+          # shell hook we manually create a virtualenv, upgrade Pip, install
+          # Poetry, then use Poetry to install dependencies. This also allows
+          # someone to use Poetry to install this Python environment without
+          # using Nix and simultaneously take advantage of the benefits of both
+          # Nix and Poetry.
+          # TODO fix versions for pip, virtualenv, and poetry.
           shellHook = ''
-            [ -d .venv ] || python -m venv .venv
-            .venv/bin/pip install --upgrade pip
-            .venv/bin/pip install poetry
-            .venv/bin/poetry install
+            # [ -d .venv ] || python -m venv .venv
+            # .venv/bin/pip install --upgrade pip
+            # .venv/bin/pip install poetry
+            # .venv/bin/poetry install
           '';
         };
       });
--- justfile
+++ justfile
@@ -1,21 +1,16 @@
+default:
+
+init-poetry:
+    [ -d .venv ] || python -m venv .venv
+    .venv/bin/pip install --upgrade pip
+    .venv/bin/pip install poetry
+    .venv/bin/poetry install
+
+clean:
+    rm -rf .venv .mypy_cache .flake8 .tmp
+
 pre-commit:
-    nixfmt --check flake.nix || nixfmt flake.nix
-    prettier --write .
-    isort --profile black .
-    flake8 dollar_lambda
-    mypy --show-error-codes --exclude=readme.py dollar_lambda
-    python -m unittest test.py
-    @just readme
-    @just docs
+    @just build
 
-readme:
-    jupytext --sync readme.py
-    DOLLAR_LAMBDA_TESTING=1 jupyter nbconvert --to markdown --execute readme.ipynb
-    prettier --write readme.md
-
-docs:
-    rm -rf docs/
-    pdoc3 --html dollar_lambda --force
-    mv html/dollar_lambda docs/
-    rm -rf html
-    prettier --write docs
+just build:
+    ./build
