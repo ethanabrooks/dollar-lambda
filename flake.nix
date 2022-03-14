bf3f9747fa98492e990129387f64043bff949def
Alec Newman <alec@rovaughn.com>
2022-03-14 09:06:39 -0400

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "";
+  inputs = { nixpkgs.url = "nixpkgs/nixos-21.11"; };
+  outputs = { self, nixpkgs }:
+    (let systems = [ "x86_64-darwin" ];
+    in {
+      packages = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = (nixpkgs.lib.genAttrs systems
+            (system: import nixpkgs { inherit system; })).${system};
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = [ pkgs.gnumake pkgs.python310 ];
+            shellHook = ''
+              [ -d .venv ] || python -m venv .venv
+              .venv/bin/pip install --upgrade pip
+              .venv/bin/pip install poetry
+              .venv/bin/poetry install
+            '';
+          };
+        });
+    });
+}
