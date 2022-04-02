6426dc9c12a2bc6e0375ee174841dcbd625edd24
Ethan Brooks <ethanabrooks@gmail.com>
2022-04-02 14:03:15 -0400

    add to docs

--- dollar_lambda/__init__.py
+++ dollar_lambda/__init__.py
@@ -692,6 +692,8 @@ user the most flexibility in terms of accessing and using the config file. Here 
 Define a parser with optional values:
 >>> p = option("x", type=int).optional()
 
+Note that parsers must be optional (so that the arguments can be ommitted on the command line) but not default
+(or else the config value will always be overwritten).
 Inside main, load the config and update with any arguments provided on the command line:
 >>> import json
 >>> def main(**kwargs):
@@ -701,11 +703,11 @@ Inside main, load the config and update with any arguments provided on the comma
 ...     config.update(kwargs)
 ...     return config
 
-Overwrite the value in the config:
+Overwrite the value in the config by providing an explicit argument:
 >>> main(**p.parse_args("-x", "0"))
 {'x': 0}
 
-Fallback to the value in the config
+Fall back to the value in the config by not providing an argument for `x`:
 >>> main(**p.parse_args())
 {'x': 1}
 
@@ -727,25 +729,43 @@ We can also write this with `@command` syntax:
 There may be cases in which a user wants to provide certain arguments on the
 command line that `$λ` should ignore (not return in the output of `Parser.parse_args`
 or pass to the a decorated function). Suppose we wish to ignore any arguments starting
-with the `--config.` prefix:
+with the `--config-` prefix:
 
->>> regex = r"config.\\S*"
+>>> regex = r"config-\\S*"
 >>> config_parsers = flag(regex) | option(regex)
+
+In the case of ordered arguments, we simply use the `ignore` method:
+
+>>> p = flag("x") >> config_parsers.ignore() >> flag("y")
+
+This will ignore any arguments that start with `--config-` and come between `x` and `y`:
+>>> p.parse_args("-x", "--config-foo", "-y")
+{'x': True, 'y': True}
+
+It also works with `option`:
+>>> p.parse_args("-x", "--config-bar", "1", "-y")
+{'x': True, 'y': True}
+
+In the case of nonpositional arguments, use the `repeated` keyword:
 >>> p = nonpositional(flag("x"), flag("y"), repeated=config_parsers.ignore())
 
-Note that neither `--config.foo` nor `--config.bar` show up in the output:
->>> p.parse_args("-x", "-y", "--config.foo", "--config.bar", "1")
+Now neither `config-foo` nor `config-bar` show up in the output:
+>>> p.parse_args("-x", "-y", "--config-foo", "--config-bar", "1")
 {'x': True, 'y': True}
 
 This works regardless of order:
->>> p.parse_args("--config.baz", "1", "-y", "--config.foz", "-x")
+>>> p.parse_args("--config-baz", "1", "-y", "--config-foz", "-x")
+{'y': True, 'x': True}
+
+And no matter how many matches are found:
+>>> p.parse_args("--config-baz", "1", "--config-blah", "-y", "--config-foz", "-x", "--config-foosh")
 {'y': True, 'x': True}
 
 The same strategy can be used with decorators:
 >>> @command(repeated=config_parsers.ignore())
 ... def f(x: bool, y: bool):
 ...    return dict(x=x, y=y)
->>> f("-x", "-y", "--config.foo", "--config.bar", "1")
+>>> f("-x", "-y", "--config-foo", "--config-bar", "1")
 {'x': True, 'y': True}
 
 And similarly with `CommandTree`.
